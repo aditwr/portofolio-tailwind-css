a99811a42016c26ee0cb19015c44e39c77814084
adityawahyuramadhan <awahyu148@gmail.com>
2022-05-29 00:32:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: 'rgb(15, 23, 42)'
+      }
+    },
+  },
+  plugins: [],
+}
